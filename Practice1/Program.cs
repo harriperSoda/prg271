a8294bd38644fae2debdb3b487738f92784a2c21
Aman <amanharripersadh12@gmail.com>
2026-08-05 17:25:54 +0200

    Added first working code from class, Day 1. I understand it. See Readme for more details on a concept

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -10,6 +10,19 @@ namespace Practice1
     {
         static void Main(string[] args)
         {
+            Dog myDog = new Dog();
+            //The class Dog is the blueprint - it defines what it should look like: name etc
+            //Dog myDog... is the consutrction.
+            //see readme for more information on this Point 1
+            
+            //Initilization of values to all fields in Dog class
+            myDog.name = "Fido";
+            myDog.age = 3;
+            myDog.height = 0.5;
+            myDog.weight = 20.0;
+
+            // Follows the object reference in memory and runs the method using that specific object's data
+            myDog.currentStatus();
         }
     }
 }
