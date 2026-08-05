b3f0f2fc6fc6f378978c62b3d56823e2ed35c49f
Aman <amanharripersadh12@gmail.com>
2026-08-05 22:07:39 +0200

    Did the exercise. No constructor

---
+++ Monday_Exercise3/GameCharacters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monday_Exercise3
+{
+    internal class GameCharacters
+    {
+        public string name;
+        public string characterClass;
+        public int health;
+
+        public void displayCharacter()
+        {
+            Console.WriteLine($"{name} is a {characterClass} with {health} health");
+        }
+    }
+    
+    }
+
---
+++ Monday_Exercise3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monday_Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameCharacters character1 = new GameCharacters();
+            character1.name = "Aragorn";
+            character1.characterClass = "Ranger";
+            character1.health = 100;
+
+            character1.displayCharacter();
+        }
+    }
+}
