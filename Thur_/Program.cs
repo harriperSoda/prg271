64ef0f271627c980f6412c4f5a4890a5e0e75845
Aman <amanharripersadh12@gmail.com>
2026-08-06 14:48:01 +0200

    Added methods calling

--- Thur_/Program.cs
+++ Thur_/Program.cs
@@ -10,6 +10,30 @@ namespace Thur_
     {
         static void Main(string[] args)
         {
+            Product product1 = new Product("Bread", 23.00, 3);
+            Product product2 = new Product("Protein Powder", 399.00, 1);
+
+            Console.WriteLine("Method Overloading");
+            Console.WriteLine("================================");
+
+            //invoke CalcualteTotal Method
+            Console.WriteLine($"Total using stored quanity: R{product1.CalculateTotal(5)}");
+
+            //CalculateTotal(int quantity)
+            Console.WriteLine($"Total for 5 loaves of bread: R{product1.CalculateTotal(5)}");
+
+            //CalcualteTotal(2 parametres)
+            Console.WriteLine($"The total for 5 loaves of bread with a 5% discount is: R{product1.CalculateTotal(5,5)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("ORIGINAL PRODUCTS");
+            Console.WriteLine("================================");
+
+            product1.displayDetails();
+            product2.displayDetails();
+
+
         }
     }
 }
