54014333c62e38994b5a0ac268777546f786e04e
Aman <amanharripersadh12@gmail.com>
2026-08-06 14:30:28 +0200

    Added method or opperator overload and some displayDetails methods

--- Thur_/Product.cs
+++ Thur_/Product.cs
@@ -8,5 +8,52 @@ namespace Thur_
 {
     internal class Product
     {
+        public string name;
+        public double price;
+        public int quanity;
+
+        public Product(string name, double price, int quanity)
+        {
+            //
+
+
+        }
+        //method 1
+        public double CalculateTotal()
+        {
+            return price * quanity;
+        }
+        //method 2
+        public double CalculateTotal(int quantity)
+        {
+            return price * quantity;
+        }
+        //method 3
+        public double CalculateTotal(int quantity, double discountPercentage)
+        {
+            double subTotal = price * quantity;
+            double discountAmount = subTotal * (discountPercentage / 100);
+            return subTotal - discountAmount;
+        }
+
+        //opperator overload
+        public static Product operator +(Product Product1, Product Product2)
+        {
+            int combinedQuantity = Product1.quanity + Product2.quanity;
+            double combinedTotalPrice = Product1.CalculateTotal() + Product2.CalculateTotal();
+            double combinedUnitPrice = combinedQuantity > 0? combinedTotalPrice / combinedQuantity : 0;
+
+            string combinedName = Product1.name + Product2.name;
+            return new Product(combinedName, combinedUnitPrice, combinedQuantity);
+        }
+        public void displayDetails()
+        {
+            Console.WriteLine($"Product Name: {name}");
+            Console.WriteLine($"Unit Price: {price}");
+            Console.WriteLine($"Quantity: {quanity}");
+            Console.WriteLine($"Total Price: {CalculateTotal()}");
+        }
     }
 }
+
+    
