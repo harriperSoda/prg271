a893b6376a20cb9775f2cc4166f1c3d61edbb2cc
Aman <amanharripersadh12@gmail.com>
2026-08-11 17:46:59 +0200

    Added Aircraft.cs

---
+++ Inheritance/Inheritance_Exercise3/Aircraft.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercise3
+{
+    internal class Aircraft:Equipment
+    {
+        public string type { get; set; }
+        public string tailNumber {get; set; }  
+
+        public Aircraft(DateTime acquireDate, string propetyID, string type, string tailNumber):base(acquireDate, propetyID)
+        {
+            this.type = type;
+            this.tailNumber = tailNumber;
+        }
+    }
+}
