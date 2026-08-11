5d81bce9e03aae16c11c46bdc1ef2925c9f33cbd
Aman <amanharripersadh12@gmail.com>
2026-08-11 17:04:10 +0200

    Created Person.cs and added propeties and constructor with logic breakdown

---
+++ Inheritance/Inheritance_Exercise3/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercise3
+{
+    internal class Person
+    {
+        //Declare our propeties 
+        public double age { get; set; }
+        public string name { get; set; }
+
+        //create the contructor for the Person class
+        public Person(double age, string name) //age and name here are not based of the above declared - they can be named anything
+        {
+            this.age = age;
+            this.name = name;
+
+            //function of this: set the class propety age to the value of the age parameter passed into the constructor. Same for name.
+        }
+    }
+}
---
+++ Inheritance/Inheritance_Exercise3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
