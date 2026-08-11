0d4ce81d172a03fcc9cb7eac1deadc2427bfad54
Aman <amanharripersadh12@gmail.com>
2026-08-11 17:42:48 +0200

    Added Equipment.cs

---
+++ Inheritance/Inheritance_Exercise3/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercise3
+{
+    internal class Equipment
+    {
+        public DateTime acquireDate { get; set; }
+        public string propetyID { get; set; }
+
+        public Equipment(DateTime acquireDate, string propetyID)
+        {
+
+        
+            this.acquireDate = acquireDate;
+            this.propetyID = propetyID;
+        }
+        
+    }
+}
