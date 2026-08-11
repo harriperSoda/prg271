e4ac10c7de6691db1d699c1b1c9e44ec2aaa1191
Aman <amanharripersadh12@gmail.com>
2026-08-11 17:13:56 +0200

    Added employee.cs with new logic : base - explained and understoof

---
+++ Inheritance/Inheritance_Exercise3/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercise3
+{
+    internal class Employee:Person
+    {
+        public DateTime dateHired { get; set; }
+        public string idNumber { get; set; }
+
+        public Employee(double age, string name, DateTime dateHired, string idNumber):base(age, name)
+        //base (age, name) calls the constructor of the base class (Person) to initialize the age and name properties inherited from Person. This ensures that when an Employee object is created, it also properly initializes the properties defined in the Person class.
+        {
+            this.dateHired = dateHired;
+            this.idNumber = idNumber;
+        }
+    }
+}
