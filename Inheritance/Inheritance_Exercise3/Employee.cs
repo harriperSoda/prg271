2caa902ff93cebc0f5e53ff0148c7e75282ec2a8
Aman <amanharripersadh12@gmail.com>
2026-08-11 18:43:18 +0200

    Added method to print stuff

--- Inheritance/Inheritance_Exercise3/Employee.cs
+++ Inheritance/Inheritance_Exercise3/Employee.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 
 namespace Inheritance_Exercise3
 {
-    internal class Employee:Person
+    internal class Employee : Person
     {
         public DateTime dateHired { get; set; }
         public string idNumber { get; set; }
 
-        public Employee(double age, string name, DateTime dateHired, string idNumber):base(age, name)
+        public Employee(double age, string name, DateTime dateHired, string idNumber) : base(age, name)
         //base (age, name) calls the constructor of the base class (Person) to initialize the age and name properties inherited from Person. This ensures that when an Employee object is created, it also properly initializes the properties defined in the Person class.
         {
             this.dateHired = dateHired;
             this.idNumber = idNumber;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}, Date Hired: {dateHired.ToShortDateString()}, ID Number: {idNumber}");
         }
     }
 }
