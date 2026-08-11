1a752c0b349af30bc55e8c7b866cf166fea6ca52
Aman <amanharripersadh12@gmail.com>
2026-08-11 18:52:45 +0200

    instantiated in program.cs

--- Inheritance/Inheritance_Exercise3/Program.cs
+++ Inheritance/Inheritance_Exercise3/Program.cs
@@ -10,6 +10,11 @@ namespace Inheritance_Exercise3
     {
         static void Main(string[] args)
         {
+            //Employee is the class. employee1 is the object. new Employee() is the constructor. The parameters are passed in to initialize the properties of the Employee object.
+            Employee employee1 = new Employee(30, "John Doe", new DateTime(2020, 5, 15), "EMP12345");
+
+            // Call the DisplayInfo method to show the employee's information
+            employee1.DisplayInfo();
         }
     }
 }
