2e76fcfc485244c954514f1bf9672a335ba673ce
Aman <amanharripersadh12@gmail.com>
2026-08-11 17:27:51 +0200

    Added Customer.cs. Usual logic

---
+++ Inheritance/Inheritance_Exercise3/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercise3
+{
+    internal class Customer
+    {
+    }
+}
