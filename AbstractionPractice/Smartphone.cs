7909d87ce736f6af5409c0a973b8f191d28ea51b
Aman <amanharripersadh12@gmail.com>
2026-08-11 12:11:00 +0200

    This is a base level. You have 2 fields. In the next step we will apply encapsulation and getters and setters

---
+++ AbstractionPractice/Smartphone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractionPractice
+{
+    internal class Smartphone
+    {
+
+        private string model;
+        public int battery;
+    }
+}
