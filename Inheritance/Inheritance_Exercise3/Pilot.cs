22c2fb81723a0d3b9b3c4448cbcb0be50dfffc06
Aman <amanharripersadh12@gmail.com>
2026-08-11 17:21:22 +0200

    Added Pilot.cs and used base for multi-level inheritance

---
+++ Inheritance/Inheritance_Exercise3/Pilot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Exercise3
+{
+    internal class Pilot:Employee
+    {
+        public DateTime certificationDate { get; set; }
+
+        public Pilot(double age, string name, DateTime dateHIred, string idNumber, DateTime certificationDate):base(age, name, dateHIred, idNumber)
+        {
+            this.certificationDate = certificationDate;
+        }
+    }
+}
