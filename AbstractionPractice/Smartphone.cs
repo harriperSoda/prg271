330c06857d5905172cc338b478a3964896c4b75d
Aman <amanharripersadh12@gmail.com>
2026-08-11 12:37:52 +0200

    Added get and set logic. I now understand

--- AbstractionPractice/Smartphone.cs
+++ AbstractionPractice/Smartphone.cs
@@ -8,8 +8,21 @@ namespace AbstractionPractice
 {
     internal class Smartphone
     {
-
+        //ideally fields should be private. We controll acees to them via enapsuation or getters and setters
         private string model;
-        public int battery;
+        private int battery;
+
+        //Here we are using properties to control access to the fields. We can also use auto-implemented properties                                                     
+        public int Battery
+        {
+            get //this just returns the value of the battery field. We can also add logic here to control access to the field
+            {
+                return battery;
+            }
+            set //this sets the value of the battery field. We can also add logic here to control access to the field
+            {
+                battery = value;
+            }
+        }
     }
 }
