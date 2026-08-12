f6243a2586eb3c780c1602b34cd33bd804f4dfe2
Aman <amanharripersadh12@gmail.com>
2026-08-12 22:17:59 +0200

    demonstrates the basics of object-oriented programming by showing how to define a simple Dog class with a constructor, create an object from that class, and access its data.

---
+++ Polymorphism/Poly_FirstPracticeRun/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poly_FirstPracticeRun
+{
+    internal class Dog
+    {
+
+        public string name;
+        public int age;
+
+        //constructor --> called everytime you create a new dog
+        //takes raw values and assigns them to the properties of the dog object
+        public Dog(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
---
+++ Polymorphism/Poly_FirstPracticeRun/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poly_FirstPracticeRun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. INSTANTIATION (CREATING AN OBJECT)
+            // We are taking our 'Dog' blueprint (the class) and building a real dog object in memory.
+            Dog dog1 = new Dog("Fido", 3);
+
+            // 2. USING THE OBJECT (ACCESSING DATA)
+            // We access the data (name and age) stored inside our specific dog object and print it.
+            Console.WriteLine($"Dog's Name: {dog1.name}");
+            Console.WriteLine($"Dog's Age: {dog1.age}");
+
+        }
+    }
+}
