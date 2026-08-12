50d68d0898dbe6f2f9e1ab3ffb58cbe5983fd2f0
Aman <amanharripersadh12@gmail.com>
2026-08-12 15:32:35 +0200

    Calculatoer app done in class

---
+++ Events_Class2/Calc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Class2
+{
+    internal class Calc
+    {
+        public static double Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static double Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Divide(int a, int b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                Console.WriteLine("Division by zero is not allowed.");
+                return 0;
+            }
+        }
+
+        //4 delegates
+        public delegate double AddDelegate(int a, int b);
+        public delegate double SubtractDelegate(int a, int b);
+        public delegate double MultiplyDelegate(int a, int b);
+        public delegate double DivideDelegate(int a, int b);
+    }
+}
---
+++ Events_Class2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Events_Class2.Calc;
+
+namespace Events_Class2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //create a class called calcilations
+            //4 methods + - * /
+            //4 delegates
+            // invoke the methods inside your main method
+
+            AddDelegate obj = new AddDelegate(Calc.Add);
+            double result = obj(10, 5);
+            Console.WriteLine("Addition: " + result);
+
+            SubtractDelegate obj2 = new SubtractDelegate(Calc.Subtract);
+            double result2 = obj2(10, 5);
+            Console.WriteLine("Subtraction: " + result2);
+
+            MultiplyDelegate obj3 = new MultiplyDelegate(Calc.Multiply);
+            double result3 = obj3(10, 5);
+            Console.WriteLine("Multiplication: " + result3);
+
+            DivideDelegate obj4 = new DivideDelegate(Calc.Divide);
+            Console.WriteLine("Division: " + obj4(10, 5));
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
