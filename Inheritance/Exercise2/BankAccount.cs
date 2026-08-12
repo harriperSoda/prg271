8e059a025653ff041a40ba0493d5abae48556afb
Aman <amanharripersadh12@gmail.com>
2026-08-12 13:44:59 +0200

    Added exercise 2. Incomplete.

---
+++ Inheritance/Exercise2/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    internal class BankAccount
+    {
+
+        private string owner { get; set; }
+        private double balance { get; set; }
+
+
+    }
+}
