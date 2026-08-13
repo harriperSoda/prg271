6f9f370f332a61361e5e8938cab048f54b3887cd
Aman <amanharripersadh12@gmail.com>
2026-08-13 09:56:02 +0200

    Added last child class. Same logic as prior 2

---
+++ Practical_Practice/PastryChef.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Practice
+{
+    internal class PastryChef: Contestant
+    {
+
+        //constructor
+        public PastryChef(int contestantNumber, string contestantName, int contestantAge, int yearsOfExperience, string team) : base(contestantNumber, contestantName, contestantAge, yearsOfExperience, team)
+        {
+        }
+
+        //override
+        public override void prepareDish()
+        {
+            Console.WriteLine($"{contestantName} is preparing a pastry dish.");
+        }
+    }
+}
