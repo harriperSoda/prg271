42e3c2299a8bbe59d97ecb590ce69bc7624b7e5e
Aman <amanharripersadh12@gmail.com>
2026-08-13 11:01:04 +0200

    Added VipService as child class

---
+++ Practical_Practice/VipService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Practice
+{
+    internal class VipService: RestaurantServices
+    {
+        //constructors
+        public VipService(int serviceNumber, int numberOfGuests, string serviceType, double timeLimit, string currentStatus) : base(serviceNumber, numberOfGuests, serviceType, timeLimit, currentStatus)
+        {
+
+        }
+
+        //orverride
+        public override void startService()
+        {
+                       Console.WriteLine($"VIP service {serviceNumber} for {numberOfGuests} guests has started. Service type: {serviceType}, Time limit: {timeLimit} hours, Current status: {currentStatus}");
+        }
+    }
+}
