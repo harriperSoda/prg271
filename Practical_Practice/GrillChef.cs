a137ffe94d85928f074c13556866b0dfc7a97823
Aman <amanharripersadh12@gmail.com>
2026-08-13 09:52:36 +0200

    Added GrillChef child class with same logic as pasteryChef child class

---
+++ Practical_Practice/GrillChef.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Practice
+{
+    internal class GrillChef: Contestant 
+    {
+        public GrillChef(int contestantNumber, string contestantName, int contestantAge, int yearsOfExperience, string team) : base(contestantNumber, contestantName, contestantAge, yearsOfExperience, team)
+        {
+        }
+        //override
+        public override void prepareDish()
+        {
+            Console.WriteLine($"{contestantName} is preparing a grilled dish.");
+        }
+    }
+}
