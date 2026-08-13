db22d8fc1d9c1725bb0493fd22f13ce1f645ce4d
Aman <amanharripersadh12@gmail.com>
2026-08-13 11:03:53 +0200

    Added last child class, ChallengeServices

---
+++ Practical_Practice/ChallengeService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Practice
+{
+    internal class ChallengeService: RestaurantServices
+    {
+
+        //constructor
+        public ChallengeService(int serviceNumber, int numberOfGuests, string serviceType, double timeLimit, string currentStatus): base(serviceNumber, numberOfGuests, serviceType, timeLimit, currentStatus)
+        {
+        }
+
+        //override
+        public override void startService()
+        {
+            Console.WriteLine($"Challenge service {serviceNumber} for {numberOfGuests} guests has started. Service type: {serviceType}, Time limit: {timeLimit} hours, Current status: {currentStatus}");
+        }
+    }
+}
