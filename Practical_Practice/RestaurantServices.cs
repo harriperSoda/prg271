109849665df86b5382b306c82fc906c6f9041a7c
Aman <amanharripersadh12@gmail.com>
2026-08-13 10:06:06 +0200

    Next step of buiod. Added abstract class for child classess to reffer too

---
+++ Practical_Practice/RestaurantServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Practice
+{
+    public abstract class RestaurantServices
+    {
+        //this will be abstract class and will be inherited by other classes
+
+        public int serviceNumber { get; set; }
+        public int numberOfGuests { get; set; }
+        public string serviceType { get; set; }
+        public double timeLimit { get; set; } //in hours
+        public string currentStatus { get; set; } //e.g. "In Progress", "Completed", "Cancelled"
+
+
+        //constructor
+
+        public RestaurantServices(int serviceNumber, int numberOFGuests, string serviceType, double timeLimit, string currentStatus)
+        {
+            this.serviceNumber = serviceNumber;
+            this.numberOfGuests = numberOFGuests;
+            this.serviceType = serviceType;
+            this.timeLimit = timeLimit;
+            this.currentStatus = currentStatus;
+
+        }
+
+        //create abstract method
+        public abstract void startService();
+    }
+}
