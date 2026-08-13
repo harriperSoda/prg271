fd967c474df1c4c4809929c014b332867ab105a4
Aman <amanharripersadh12@gmail.com>
2026-08-13 10:10:55 +0200

    Added child class that refference abstract class. Constructror with base refference and override method

---
+++ Practical_Practice/StandardService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Practice
+{
+    internal class StandardService: RestaurantServices
+    {
+
+        //constructor
+        public StandardService(int serviceNumber, int NumberofGuests, string serviceType, double timeLimit, string currentStatus) : base(serviceNumber, NumberofGuests, serviceType, timeLimit, currentStatus)
+        {
+        }
+
+        public override void startService()
+        {
+            Console.WriteLine($"Standard service {serviceNumber} for {numberOfGuests} guests has started. Service type: {serviceType}, Time limit: {timeLimit} hours, Current status: {currentStatus}");
+        }
+    }
+}
