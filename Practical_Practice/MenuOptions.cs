5d384130394627869f3e4e40081dd91ea519eb58
Aman <amanharripersadh12@gmail.com>
2026-08-13 13:33:40 +0200

    Created enum class and started making enum switch case in Main

---
+++ Practical_Practice/MenuOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Practice
+{
+
+        public enum MenuOptions
+        {
+            RegisterPastryChef = 1,
+            RegisterGrillChef = 2,
+            ViewAllContestants = 3,
+            StartStandardService = 4,
+            StartVipService = 5,
+            RecordIncident = 6,
+            Exit = 7
+        }
+    
+}
--- Practical_Practice/Program.cs
+++ Practical_Practice/Program.cs
@@ -10,6 +10,72 @@ namespace Practical_Practice
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Opening Hell's Kitchen...");
+
+            // 2. CREATE YOUR LISTS HERE (Before the loop!)
+            List<Contestant> contestantList = new List<Contestant>();
+            List<RestaurantServices> serviceList = new List<RestaurantServices>();
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("\nMenu Options:");
+                Console.WriteLine("1. Register Pastry Chef");
+                Console.WriteLine("2. Register Grill Chef");
+                Console.WriteLine("3. View All Contestants");
+                Console.WriteLine("4. Start Standard Service");
+                Console.WriteLine("5. Start VIP Service");
+                Console.WriteLine("6. Record Incident");
+                Console.WriteLine("7. Exit");
+
+                //read inputs 
+                if (int.TryParse(Console.ReadLine(), out int inputtedOption))
+                {
+                       //Cast integer to the enum
+                    MenuOptions choice = (MenuOptions)inputtedOption;
+
+                    //Switch on enum
+                    switch (choice)
+                    {
+                        case MenuOptions.RegisterPastryChef:
+                            PastryChef pChef = new PastryChef(1, "Chef Madhir", 22, 3, "blue");
+                            contestantList.Add(pChef);
+                            Console.WriteLine("SUCCESS: Pastry Chef added!");
+                            break;
+
+                        case MenuOptions.RegisterGrillChef:
+                            GrillChef gChef = new GrillChef(2, "Chef Ramesh", 24, 3, "Blue");
+                            contestantList.Add(gChef);
+                            Console.WriteLine("SUCCESS: Grill Chef added!");
+                            break;
+
+                        case MenuOptions.ViewAllContestants:
+                            Console.WriteLine("\nAll Registed Contestant");
+                                if (contestantList.Count == 0)
+                            {
+                                Console.WriteLine("No contestants");
+                            }
+                            else
+                            {
+                                foreach (Contestant chef in contestantList)
+                                {
+                                    chef.prepareDish();
+                                }
+                                
+                            }
+                            break;
+                            
+
+
+
+                    }
+
+                }
+                
+
+            }
         }
     }
 }
