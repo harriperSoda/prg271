ff1448a351e21b398bad41585bb73e77337a5e8d
Aman <amanharripersadh12@gmail.com>
2026-08-13 09:25:49 +0200

    Creaated abstract contestant class. Made constructor and added the abstract method that child classes will reffer too

---
+++ Practical_Practice/Contestant.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Practice
+{
+    
+        //creation of general/asbtract classes - child classes will inherit from this class
+
+        public abstract class Contestant
+        {
+            public int contestantNumber { get; set; }
+            public string contestantName { get; set; }
+            public int contestantAge { get; set; }
+            public int yearsOfExperience { get; set; }
+            public string team { get; set; }
+
+            //constructor
+            public Contestant(int contestantNumber, string contestantName, int contestantAge, int yearsOfExperience, string team)
+            {
+                this.contestantNumber = contestantNumber;
+                this.contestantName = contestantName;
+                this.contestantAge = contestantAge;
+                this.yearsOfExperience = yearsOfExperience;
+                this.team = team;
+
+            }
+
+        //create abstract method
+        public abstract void prepareDish();
+        }
+    }
+
+        
+
