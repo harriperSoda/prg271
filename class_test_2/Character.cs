a24c3a3f40b28407c0877fe4933834d5893e991f
BC-STUDENT <BC-STUDENT@WIN-JC8F9KSIKU1>
2026-08-14 13:59:10 +0200

    CHARACTER ABS CLASS

---
+++ class_test_2/Character.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_test_2
+{
+    public abstract class Character
+    {
+
+        public int characterID { get; set; }
+        public string name { get;set; }
+        public int age { get; set; }
+        public int strengthLevel { get; set; }
+
+        public Character(int characterID, string name, int age, int strengthLevel)
+        {
+            this.characterID = characterID;
+            this.name = name;
+            this.age = age;
+            this.strengthLevel = strengthLevel;
+        }
+
+        public abstract void SummonCharacter();
+    }
+}
