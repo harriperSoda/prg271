f6768c15ab47a85667e5190ae0b4866b55319548
BC-STUDENT <BC-STUDENT@WIN-JC8F9KSIKU1>
2026-08-14 14:22:19 +0200

    mission class

---
+++ class_test_2/MIssion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_test_2
+{
+    internal class MIssion
+    {
+
+        public int missionID { get; set; }
+        public string missionNAme { get; set; }
+        public string location { get; set; }
+        public int dangerLevel { get; set; }
+        public int completionStatus { get; set; }
+
+        public MIssion(int missionID, string missionName, string location, int dangerLevel, int completionStatus)
+        {
+            this.missionID = missionID;
+            this.missionNAme = missionNAme;
+            this.location = location;
+            this.dangerLevel = dangerLevel;
+            this.completionStatus = completionStatus;
+        }
+    }
+}
