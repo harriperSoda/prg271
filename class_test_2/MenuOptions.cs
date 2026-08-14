cc61d38e5910c16358daabfccaa4a21b839a5ae9
BC-STUDENT <BC-STUDENT@WIN-JC8F9KSIKU1>
2026-08-14 14:14:06 +0200

    menu options

---
+++ class_test_2/MenuOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_test_2
+{
+    public enum MenuOptions
+    {
+        AddCharacter = 1,
+        DisplayCharacters = 2,
+        AddMission = 3,
+        DisplayMIssions = 4,
+        AddThreat = 5,
+        StartOperations = 6,
+        Exit = 7
+    }
+}
