efeac7610271ecf264224634106876d3fc6ea5c9
Aman <amanharripersadh12@gmail.com>
2026-08-20 11:18:45 +0200

    Created CrewMemebrs class for encapsulation pruposes

---
+++ 5. Windows Forms/ST_PracticeForm/CrewMember.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST_PracticeForm
+{
+    abstract class CrewMember
+    {
+
+        //propeties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+        //constructor
+        public CrewMember(int Id, string Name, string Role)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Role = Role;
+        }
+
+        //abstract method
+        public abstract void PerformDuty();
+    }
+}
