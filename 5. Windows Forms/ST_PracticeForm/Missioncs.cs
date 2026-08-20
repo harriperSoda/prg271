acfdde813218604a347717a75862b0e67d9eace8
Aman <amanharripersadh12@gmail.com>
2026-08-20 12:32:35 +0200

    Added mission class

---
+++ 5. Windows Forms/ST_PracticeForm/Missioncs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST_PracticeForm
+{
+    internal class Missioncs
+    {
+
+        //propeties
+        public string MissionName { get; set; }
+        public string Status { get; set; }
+
+        //constructor
+        public Missioncs(string MissionName, string Status)
+        {
+            this.Status = Status;
+            this.MissionName = MissionName;
+        }
+    }
+}
