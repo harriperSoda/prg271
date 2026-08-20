6cefad121bbc8de412cfa4d878fd83daf6ea5ad5
Aman <amanharripersadh12@gmail.com>
2026-08-20 12:08:00 +0200

    Created Datahander that containts the list, population of data to the list and a methoid to access the private list in Form

---
+++ 5. Windows Forms/ST_PracticeForm/CrewDataHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST_PracticeForm
+{
+    internal class CrewDataHandler
+    {
+        //creates a list allowing objects of type CrewMember to be stored in it
+        private List<CrewMember> crewMembers = new List<CrewMember>();
+
+        //method to add a crew member to the list
+        public void PopulateCrew()
+        {
+            crewMembers.Add(new Officer(1, "Odyessus", "Officer", "Captain", "Command"));
+            crewMembers.Add(new Sailor(2, "Eurycolus", "Sailor", "Deck"));
+            crewMembers.Add(new Sailor(3, "Polites", "Sailor", "Deck"));
+        }
+
+        //need the form to access the private list so create a public method to return the list
+        public List<CrewMember> getCrewMemebrs()
+        {
+            return crewMembers;
+        }
+    }
+}
